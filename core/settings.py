a9b393e7a3b90ca8a2fd333aa523c5271cc328de
AnastasiiaBelianskaia <anastasiiabelianskaia@gmail.com>
2023-05-04 19:03:12 +0300

    changed settings

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['www.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost',
+                 '127.0.0.1', ]
 
 INTERNAL_IPS = [
     'localhost',
